a8b1ffdcee6290c50ce23dd6aeef4d90f91e2580
Ben S <bsadick@gmail.com>
2018-06-14 22:59:03 +0900

    updated page

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+window.onload = checkWidth;
+window.onresize = checkWidth;
+
+function checkWidth() {
+  let w = window.innerWidth;
+  let part1 = document.getElementById("project1-desc-p1");
+  let part2 = document.getElementById("project1-desc-p2");
+
+  console.log(w);
+  if (w > 800) {
+    // part1.classList.add("");
+  }
+}
