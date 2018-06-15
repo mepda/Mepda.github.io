7206eb2ecbb96c16b7e39f07bf84524a1aa19e9f
Ben S <bsadick@gmail.com>
2018-06-15 10:20:11 +0900

    added toast

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,15 @@
-window.onload = checkWidth;
-window.onresize = checkWidth;
+// window.onload = checkWidth;
+// window.onresize = checkWidth;
 
-function checkWidth() {
-  let w = window.innerWidth;
-  let part1 = document.getElementById("project1-desc-p1");
-  let part2 = document.getElementById("project1-desc-p2");
+// function checkWidth() {
+//   let w = window.innerWidth;
+//   let part1 = document.getElementById("project1-desc-p1");
+//   let part2 = document.getElementById("project1-desc-p2");
 
-  console.log(w);
-  if (w > 800) {
-    // part1.classList.add("");
-  }
-}
+//   console.log(w);
+//   if (w > 800) {
+//     // part1.classList.add("");
+//   }
+// }
+
+M.toast({ html: "Nice!" });
