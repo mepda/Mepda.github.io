4fcb7817186a89051e10f8e561438299d061dffa
Ben S <bsadick@gmail.com>
2018-11-15 10:24:25 +0900

    added drift

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,6 @@
-console.log(
-  '%cWelcome to my %cportfolio.',
-  'color:blue; font-size:1.5em;',
-  'color:green; font-weight:bold; font-size:1.5em;'
-);
+let BLUE_BIG_FONT = 'color:blue; font-size:1.5em;';
+let BLACK_BIG_FONT = 'color:black; font-weight:bold; font-size:1.5em;';
+console.log('%cWelcome to my %cportfolio.', BLUE_BIG_FONT, BLACK_BIG_FONT);
 
 $('#contact-form').submit(function(e) {
   let email = document.getElementById('email-input');
@@ -49,13 +47,14 @@ observer.observe(myAnimation);
 
 const headerAnimation = document.querySelectorAll('.mycon');
 observerHeader = new IntersectionObserver(entries => {
+  console.log(entries);
   entries.forEach(entry => {
     if (entry.intersectionRatio > 0) {
       console.log(`entered view of ${entry}`);
       setTimeout(() => {
         console.log(entry.target.style.visibility);
         entry.target.style.visibility = 'visible';
-        entry.target.classList.add('animated-icons');
+        entry.target.classList.add('animated');
         entry.target.classList.add('bounceIn');
       }, 60);
     } else {
@@ -69,34 +68,17 @@ headerAnimation.forEach(header => {
   observerHeader.observe(header);
 });
 
-//get top position of various sections
-
-// let projectOnePosition = document
-//   .getElementById("project1")
-//   .getBoundingClientRect().top;
-// let projectTwoPosition = document
-//   .getElementById("project2")
-//   .getBoundingClientRect().top;
-// let contactPosition = document.getElementById("contact").getBoundingClientRect()
-//   .top;
-
-// //get links by id
-// let projectOneLink = document.getElementById("projectOneLink");
-// let projectTwoLink = document.getElementById("projectTwoLink");
-// let contactLink = document.getElementById("contactLink");
-
-// //adjust for navbar offset
-// projectOneLink.addEventListener("click", e => {
-//   console.log(projectOnePosition);
-//   e.preventDefault();
-//   window.scrollTo(0, projectOnePosition - 300);
+// const stick = document.getElementById('scrolld');
+// const startSticky = document.getElementById('nav-starter');
+// navObserver = new IntersectionObserver((entry, observer) => {
+//   if (entry[0].intersectionRatio > 0) {
+//     console.log('in view');
+//     stick.style.display = 'block';
+//     stick.style.visibility = 'visible';
+//   } else {
+//     console.log('out of view');
+//     stick.style.display = 'none';
+//     stick.style.visibility = 'hidden';
+//   }
 // });
-
-// $("#projectOneLink").click(function() {
-//   $("html, body").animate(
-//     {
-//       scrollTop: $("#project1").offset().top
-//     },
-//     2000
-//   );
-// });
+// navObserver.observe(startSticky);
